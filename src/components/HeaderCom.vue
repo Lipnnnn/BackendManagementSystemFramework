cc76a6555925c669d77d15e6fb227cb3b8cb0415
李佩妮 <lipn2002@163.com>
2025-11-20 19:16:12 +0800

    项目初始化

---
+++ src/components/HeaderCom.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>网络安全协调指挥平台-资产填报工具</h1>
+    <p>已填写内容为缓存数据，关闭工具前请导出数据，直接关闭工具会造成已填写数据丢失！！！</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped></style>
